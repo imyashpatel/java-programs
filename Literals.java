ad86e9d8f76f13c737fec437d1a5613e1f40d9f4
Yash prakashbhai patel <56687394+imyashpatel@users.noreply.github.com>
2022-11-18 12:44:51 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,16 @@
+public class Literals {
+    public static void main(String[] args) {
+        int n = 23;
+        byte age = 45;
+        short age3 =90;
+        float f1 = 8.9f;
+        double d1 = 7.8D;
+        boolean e = true;
+        char c1 = 'Y';
+        System.out.println(age);
+        String str = "Yash";
+        System.out.println(str);
+
+
+    }
+}
