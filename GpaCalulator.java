65b63afe514e5cf7257155fbfc10e34445e59b3d
Yash prakashbhai patel <56687394+imyashpatel@users.noreply.github.com>
2022-11-22 21:34:03 +0530

    Update GpaCalulator.java
    added more features

--- GpaCalulator.java
+++ GpaCalulator.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 public class GpaCalulator {
     public static void main(String[] args) {
+        float total = 0;
         System.out.println("Grades Point Average Calculator ");
         Scanner sc = new Scanner(System.in);
         System.out.println("enter marks of subject 1:");
         float a = sc.nextFloat();
+        total = total + a;
         System.out.println("enter marks of subject 2:");
         float b = sc.nextFloat();
+        total = total + b;
         System.out.println(" enter marks of subject 3:");
         float c = sc.nextFloat();
+        total = total + c;
 // forumla for finding grades per average of three subjects
-        float cgpa = (a+b+c)/30;
+        float cgpa = (total)/30;
         System.out.println("cgpa:" +cgpa);
 
     }
