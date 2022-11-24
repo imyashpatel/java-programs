926a25661bb66a492bb9ef0de1cfa3ea72a9ee50
Yash prakashbhai patel <56687394+imyashpatel@users.noreply.github.com>
2022-11-24 16:11:21 +0530

    Add files via upload

---
+++ All_Strings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class All_Strings {
+    public static void main(String[] args) {
+        //String name = "yash";
+        //System.out.print("the name is:");
+        //System.out.println(name);
+        //int a = 10;
+        //float b = 6.7f;
+
+        //system.out.printf("the value of a is %d and b is %f",a,b);
+        Scanner sc = new Scanner(System.in);
+        //String st = sc.next();
+        String st = sc.nextLine();
+        System.out.println(st);
+
+    }
+}
