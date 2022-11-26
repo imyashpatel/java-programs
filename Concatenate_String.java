2917fbecbc615b123f849ef76ebb0a772a69913c
Yash prakashbhai patel <56687394+imyashpatel@users.noreply.github.com>
2022-11-26 11:35:38 +0530

    Add files via upload

---
+++ Concatenate_String.java
@@ -0,0 +1,7 @@
+public class Concatenate_String {
+    public static void main(String[] args) {
+        String firstname = "yash";
+        String lastname = " patel";
+        System.out.println(firstname+lastname);
+    }
+}
