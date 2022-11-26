0e79bec8c2435edfd8e4f589e8568c5847becb7a
Yash prakashbhai patel <56687394+imyashpatel@users.noreply.github.com>
2022-11-26 11:52:31 +0530

    Add files via upload

---
+++ Con_String2.java
@@ -0,0 +1,7 @@
+public class Con_String2 {
+    public static void main(String[] args) {
+        String fname = "yash";
+        String lname = "patel";
+        System.out.println(fname.concat(lname));
+    }
+}
---
+++ Return_Index.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class Return_Index {
+    public static void main(String[] args) {
+        String txt = "Hello Everybody";
+        System.out.println(txt.indexOf("e"));
+
+    }
+}
