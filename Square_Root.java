b3fe4989f5e928d282394ae252a22250bb2d0bb4
Yash prakashbhai patel <56687394+imyashpatel@users.noreply.github.com>
2022-11-26 12:02:09 +0530

    Add files via upload

---
+++ Square_Root.java
@@ -0,0 +1,7 @@
+public class Square_Root {
+    public static void main(String[] args) {
+        int x = 16;
+        double y = Math.sqrt(x);
+        System.out.println("the square root of " +x+ "is:"+y);
+    }
+}
